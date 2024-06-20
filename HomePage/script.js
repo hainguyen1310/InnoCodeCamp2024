262bd898a6498d184d46ed1e1db35700d7f4e022
Gia Huy <trandinhgiahuy2004@gmail.com>
2024-06-20 13:22:01 +0700

    done parallax

--- HomePage/script.js
+++ HomePage/script.js
@@ -30,29 +30,24 @@
   window.addEventListener("resize", callbackFunc);
   window.addEventListener("scroll", callbackFunc);
 })();
-(function($) {
-  $.fn.timeline = function() {
+(function ($) {
+  $.fn.timeline = function () {
     var selectors = {
       id: $(this),
       item: $(this).find(".timeline-item"),
       activeClass: "timeline-item--active",
-      img: ".timeline__img"
+      img: ".timeline__img",
     };
     selectors.item.eq(0).addClass(selectors.activeClass);
     selectors.id.css(
       "background-image",
-      "url(" +
-        selectors.item
-          .first()
-          .find(selectors.img)
-          .attr("src") +
-        ")"
+      "url(" + selectors.item.first().find(selectors.img).attr("src") + ")"
     );
     var itemLength = selectors.item.length;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       var max, min;
       var pos = $(this).scrollTop();
-      selectors.item.each(function(i) {
+      selectors.item.each(function (i) {
         min = $(this).offset().top;
         max = $(this).height() + $(this).offset().top;
         var that = $(this);
@@ -60,22 +55,13 @@
           selectors.item.removeClass(selectors.activeClass);
           selectors.id.css(
             "background-image",
-            "url(" +
-              selectors.item
-                .last()
-                .find(selectors.img)
-                .attr("src") +
-              ")"
+            "url(" + selectors.item.last().find(selectors.img).attr("src") + ")"
           );
           selectors.item.last().addClass(selectors.activeClass);
         } else if (pos <= max - 60 && pos >= min) {
           selectors.id.css(
             "background-image",
-            "url(" +
-              $(this)
-                .find(selectors.img)
-                .attr("src") +
-              ")"
+            "url(" + $(this).find(selectors.img).attr("src") + ")"
           );
           selectors.item.removeClass(selectors.activeClass);
           $(this).addClass(selectors.activeClass);
@@ -87,3 +73,22 @@
 
 $("#timeline-1").timeline();
 
+(function () {
+  let bgImg = document.querySelector(".bg-img");
+  let underSmoke = document.getElementById("under-smoke");
+  let topSmoke = document.getElementById("top-smoke");
+  window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+
+    if (value >= 750 && value <= 3000) {
+      // Adjust the value as needed
+      var scale = 0.9 + value / 5000; // Adjust the scaling factor as needed
+      bgImg.style.transform = "scale(" + scale + ")";
+      underSmoke.style.marginTop = value * 0.2 + "px";
+      topSmoke.style.marginTop = -value * 0.2 + "px";
+    } else {
+      bgImg.style.transform = "scale(1)";
+      
+    }
+  });
+})();
